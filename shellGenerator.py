fe54b45d47ab9e1287218ba497748a26adba12c5
Nicolas <niconeuman@gmail.com>
2018-11-26 07:40:29 +0100

    Add files via upload

---
+++ shellGenerator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 11:08:41 2018
+
+@author: nneuman
+"""
+
+import numpy as np
+import vrrGenerator
+import ascsGenerator
+import hrrGenerator
+
+
+
+
+
